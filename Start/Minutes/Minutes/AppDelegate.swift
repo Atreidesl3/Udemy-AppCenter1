6bb6f603628a8e7cf59c1285b039115b001ca89f
Pawarit-PP <pawarit.pp@gmail.com>
2018-07-24 17:37:49 +0700

    Add AppCenter framework and link app secret

--- Start/Minutes/Minutes/AppDelegate.swift
+++ Start/Minutes/Minutes/AppDelegate.swift
@@ -1,29 +1,39 @@
+
 import UIKit
+import AppCenter
+import AppCenterDistribute
+import AppCenterCrashes
+import AppCenterAnalytics
+import AppCenterPush
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate
-{
-    static var entries: FileEntryStore = FileEntryStore()
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  static var entries: FileEntryStore = FileEntryStore()
+  
+  var window: UIWindow?
+  
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
-        return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-    }
+    MSAppCenter.start("6aff37d3-a4c1-452a-b645-587e36dec9ab",
+                      withServices: [MSDistribute.self, MSCrashes.self, MSAnalytics.self, MSPush.self])
+    
+    return true
+  }
+  
+  func applicationWillResignActive(_ application: UIApplication) {
+  }
+  
+  func applicationDidEnterBackground(_ application: UIApplication) {
+  }
+  
+  func applicationWillEnterForeground(_ application: UIApplication) {
+  }
+  
+  func applicationDidBecomeActive(_ application: UIApplication) {
+  }
+  
+  func applicationWillTerminate(_ application: UIApplication) {
+  }
+  
 }
